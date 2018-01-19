d1ae1e747767cf2006e9e8a6c01902fd3d0590ad
SubrahmanyaG <subramanya.g@hashedin.com>
2018-01-20 01:48:28 +0530

    Initial Commit using django-project-template

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hi-325/happyhelp/django-project-template-master/pinit/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
